e291c46ae81c76b9eef6a4ee0594ffa3ccf8d7d2
Łukasz Pitrus <l.pitrus@praktyki.ideo.pl>
2021-11-07 18:27:17 +0100

    .

--- MMult_4x4_16.c
+++ MMult_4x4_16.c
@@ -35,13 +35,13 @@ void MY_MMult( int m, int n, int k, double *a, int lda,
   }
 }
 
-void InnerKernel( int m, int n, int k, double *a, int lda, 
+	void InnerKernel( int m, int n, int k, double *a, int lda, 
                                        double *b, int ldb,
                                        double *c, int ldc, int first_time )
 {
   int i, j;
 
-  double 
+ 	  double 
     packedA[ m * k ];
   static double 
     packedB[ kc*nb ];    /* Note: using a static buffer is not thread safe... */
@@ -82,7 +82,7 @@ void PackMatrixB( int k, double *b, int ldb, double *b_to )
   double 
     *b_i0_pntr = &B( 0, 0 ), *b_i1_pntr = &B( 0, 1 ),
     *b_i2_pntr = &B( 0, 2 ), *b_i3_pntr = &B( 0, 3 );
-
+    
   for( i=0; i<k; i++){  /* loop over rows of B */
     *b_to++ = *b_i0_pntr++;
     *b_to++ = *b_i1_pntr++;
@@ -95,12 +95,20 @@ void PackMatrixB( int k, double *b, int ldb, double *b_to )
 #include <xmmintrin.h>  // SSE
 #include <pmmintrin.h>  // SSE2
 #include <emmintrin.h>  // SSE3
+#include <immintrin.h>
 
 typedef union
 {
   __m128d v;
   double d[2];
 } v2df_t;
+
+typedef union
+{
+  __m256d v;
+  __m128d m128[2];
+  double d[4];
+} v4df_t;
 
 void AddDot4x4( int k, double *a, int lda,  double *b, int ldb, double *c, int ldc )
 {
@@ -120,56 +128,42 @@ void AddDot4x4( int k, double *a, int lda,  double *b, int ldb, double *c, int l
      And now we use vector registers and instructions */
 
   int p;
-  v2df_t
-    c_00_c_10_vreg,    c_01_c_11_vreg,    c_02_c_12_vreg,    c_03_c_13_vreg,
-    c_20_c_30_vreg,    c_21_c_31_vreg,    c_22_c_32_vreg,    c_23_c_33_vreg,
-    a_0p_a_1p_vreg,
-    a_2p_a_3p_vreg,
-    b_p0_vreg, b_p1_vreg, b_p2_vreg, b_p3_vreg; 
 
-  c_00_c_10_vreg.v = _mm_setzero_pd();   
-  c_01_c_11_vreg.v = _mm_setzero_pd();
-  c_02_c_12_vreg.v = _mm_setzero_pd(); 
-  c_03_c_13_vreg.v = _mm_setzero_pd(); 
-  c_20_c_30_vreg.v = _mm_setzero_pd();   
-  c_21_c_31_vreg.v = _mm_setzero_pd();  
-  c_22_c_32_vreg.v = _mm_setzero_pd();   
-  c_23_c_33_vreg.v = _mm_setzero_pd(); 
+  v4df_t a_0p_a_1p_vreg_a_2p_a_3p_vreg, c_00_c_10_vreg_c_20_c_30_vreg,c_01_c_11_vreg_c_21_c_31_vreg,c_02_c_12_vreg_c_22_c_32_vreg,
+  c_03_c_13_vreg_c_23_c_33_vreg,b_p0_vreg256, b_p1_vreg256, b_p2_vreg256, b_p3_vreg256;
+    
+  c_00_c_10_vreg_c_20_c_30_vreg.v = _mm256_setzero_pd();   
+  c_01_c_11_vreg_c_21_c_31_vreg.v = _mm256_setzero_pd();
+  c_02_c_12_vreg_c_22_c_32_vreg.v = _mm256_setzero_pd(); 
+  c_03_c_13_vreg_c_23_c_33_vreg.v = _mm256_setzero_pd(); 
 
   for ( p=0; p<k; p++ ){
-    a_0p_a_1p_vreg.v = _mm_load_pd( (double *) a );
-    a_2p_a_3p_vreg.v = _mm_load_pd( (double *) ( a+2 ) );
-    a += 4;
+    a_0p_a_1p_vreg_a_2p_a_3p_vreg.v=_mm256_loadu_pd( (double *) a );
+    a+=4;
 
-    b_p0_vreg.v = _mm_loaddup_pd( (double *) b );       /* load and duplicate */
-    b_p1_vreg.v = _mm_loaddup_pd( (double *) (b+1) );   /* load and duplicate */
-    b_p2_vreg.v = _mm_loaddup_pd( (double *) (b+2) );   /* load and duplicate */
-    b_p3_vreg.v = _mm_loaddup_pd( (double *) (b+3) );   /* load and duplicate */
+    b_p0_vreg256.v =_mm256_set_pd(*b,*b,*b,*b);
+    b_p1_vreg256.v =_mm256_set_pd(*(b+1),*(b+1),*(b+1),*(b+1));
+    b_p2_vreg256.v =_mm256_set_pd(*(b+2),*(b+2),*(b+2),*(b+2));
+    b_p3_vreg256.v =_mm256_set_pd(*(b+3),*(b+3),*(b+3),*(b+3));
 
-    b += 4;
+    b+=4;
 
-    /* First row and second rows */
-    c_00_c_10_vreg.v += a_0p_a_1p_vreg.v * b_p0_vreg.v;
-    c_01_c_11_vreg.v += a_0p_a_1p_vreg.v * b_p1_vreg.v;
-    c_02_c_12_vreg.v += a_0p_a_1p_vreg.v * b_p2_vreg.v;
-    c_03_c_13_vreg.v += a_0p_a_1p_vreg.v * b_p3_vreg.v;
+    c_00_c_10_vreg_c_20_c_30_vreg.v+=a_0p_a_1p_vreg_a_2p_a_3p_vreg.v*b_p0_vreg256.v;
+    c_01_c_11_vreg_c_21_c_31_vreg.v+=a_0p_a_1p_vreg_a_2p_a_3p_vreg.v*b_p1_vreg256.v;
+    c_02_c_12_vreg_c_22_c_32_vreg.v+=a_0p_a_1p_vreg_a_2p_a_3p_vreg.v*b_p2_vreg256.v;
+    c_03_c_13_vreg_c_23_c_33_vreg.v+=a_0p_a_1p_vreg_a_2p_a_3p_vreg.v*b_p3_vreg256.v;
 
-    /* Third and fourth rows */
-    c_20_c_30_vreg.v += a_2p_a_3p_vreg.v * b_p0_vreg.v;
-    c_21_c_31_vreg.v += a_2p_a_3p_vreg.v * b_p1_vreg.v;
-    c_22_c_32_vreg.v += a_2p_a_3p_vreg.v * b_p2_vreg.v;
-    c_23_c_33_vreg.v += a_2p_a_3p_vreg.v * b_p3_vreg.v;
   }
 
-  C( 0, 0 ) += c_00_c_10_vreg.d[0];  C( 0, 1 ) += c_01_c_11_vreg.d[0];  
-  C( 0, 2 ) += c_02_c_12_vreg.d[0];  C( 0, 3 ) += c_03_c_13_vreg.d[0]; 
+  C( 0, 0 ) += c_00_c_10_vreg_c_20_c_30_vreg.d[0];  C( 0, 1 ) += c_01_c_11_vreg_c_21_c_31_vreg.d[0];  
+  C( 0, 2 ) += c_02_c_12_vreg_c_22_c_32_vreg.d[0];  C( 0, 3 ) += c_03_c_13_vreg_c_23_c_33_vreg.d[0]; 
 
-  C( 1, 0 ) += c_00_c_10_vreg.d[1];  C( 1, 1 ) += c_01_c_11_vreg.d[1];  
-  C( 1, 2 ) += c_02_c_12_vreg.d[1];  C( 1, 3 ) += c_03_c_13_vreg.d[1]; 
+  C( 1, 0 ) += c_00_c_10_vreg_c_20_c_30_vreg.d[1];  C( 1, 1 ) += c_01_c_11_vreg_c_21_c_31_vreg.d[1];  
+  C( 1, 2 ) += c_02_c_12_vreg_c_22_c_32_vreg.d[1];  C( 1, 3 ) += c_03_c_13_vreg_c_23_c_33_vreg.d[1]; 
 
-  C( 2, 0 ) += c_20_c_30_vreg.d[0];  C( 2, 1 ) += c_21_c_31_vreg.d[0];  
-  C( 2, 2 ) += c_22_c_32_vreg.d[0];  C( 2, 3 ) += c_23_c_33_vreg.d[0]; 
+  C( 2, 0 ) += c_00_c_10_vreg_c_20_c_30_vreg.d[2];  C( 2, 1 ) += c_01_c_11_vreg_c_21_c_31_vreg.d[2];  
+  C( 2, 2 ) += c_02_c_12_vreg_c_22_c_32_vreg.d[2];  C( 2, 3 ) += c_03_c_13_vreg_c_23_c_33_vreg.d[2]; 
 
-  C( 3, 0 ) += c_20_c_30_vreg.d[1];  C( 3, 1 ) += c_21_c_31_vreg.d[1];  
-  C( 3, 2 ) += c_22_c_32_vreg.d[1];  C( 3, 3 ) += c_23_c_33_vreg.d[1]; 
+  C( 3, 0 ) += c_00_c_10_vreg_c_20_c_30_vreg.d[3];  C( 3, 1 ) += c_01_c_11_vreg_c_21_c_31_vreg.d[3];  
+  C( 3, 2 ) += c_02_c_12_vreg_c_22_c_32_vreg.d[3];  C( 3, 3 ) += c_03_c_13_vreg_c_23_c_33_vreg.d[3]; 
 }
